4b497a3c8fa6f85826f86614fcb7ac2838f8f6e7
Bence Lovas <bencelovas@Bences-MacBook-Air.local>
2020-11-29 01:50:21 +0100

    rotate cards on keypress

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TODO
-- add card types
-- make difference between top - right city one or separated
+- ability to rotate cards
+- when placing cards it should be a valid place
+- it is possible that the full city card cannot be played
+
+
+- make difference between top - right city one or separated (for now it is good but maybe it will cause other issues)
--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { getShuffledCards } from './cards/cardController'
-import {useState} from 'react';
+import {useState, useEffect, useCallback} from 'react';
 
 const cards = getShuffledCards();
 
 function App() {
   const [currentCardIndex, setCurrentCardIndex] = useState(0)
   const [cardsOnTable, setCardsOnTable] = useState([])
+  const [rotation, setRotation] = useState(0)
 
   const createTable = () => {
     const rectagles = []
@@ -18,18 +19,40 @@ function App() {
     return rectagles;
   }
 
+  const rotateCard = () => {
+    setRotation((prev) => (prev + 90) % 360)
+  }
+
   const onClick = (evt) => {
     const x = Math.floor(evt.nativeEvent.layerX / 100) * 100
     const y = Math.floor(evt.nativeEvent.layerY / 100) * 100
     setCardsOnTable([
       ...cardsOnTable, 
       <svg x={x} y={y} width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-        <rect width="100%" height="100%" fill="#ddd" /> 
+        <rect width="100%" height="100%" fill="rgba(132, 197, 186, 0.52)"/> 
+        <g transform={'rotate(' + rotation + ')'} transform-origin="center">
         {cards[currentCardIndex].elements}
+
+        </g>
       </svg>
     ])
-
+    setCurrentCardIndex(currentCardIndex + 1)
   }
+
+  const keyDownHandler = (event) => {
+    // 82 = r
+    if(event.keyCode === 82) {
+      rotateCard();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyDownHandler, false);
+
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler, false);
+    };
+  }, []);
 
   return (
     <div style={{ position: 'relative', marginLeft: 300}}>
@@ -37,10 +60,13 @@ function App() {
       <div>
         <div>Current Card</div>
         <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-          <rect width="100%" height="100%" fill="#ddd" /> 
-          {cards[currentCardIndex].elements}
+          <rect width="100%" height="100%" fill="rgba(132, 197, 186, 0.52)" /> 
+          <g transform={'rotate(' + rotation + ')'} transform-origin="center">
+            {cards[currentCardIndex].elements}
+
+          </g>
         </svg>
-        <button onClick={() => setCurrentCardIndex(currentCardIndex + 1)} disabled={currentCardIndex >= cards.length - 1}>NEXT</button>
+        <button onClick={() => rotateCard()}>ROTATE (R)</button>
       </div>
       <div style={{ position: 'absolute'}} >
         <svg width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">
